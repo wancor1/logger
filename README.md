8349a6102f26b40598c178cf02223eea1443b48f
わん <wanko.inuco@gmail.com>
2026-03-22 16:26:10 +0900

    add file output

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
 # logger
 
 Outputs the log.
-
-<img width="737" height="119" alt="image" src="https://github.com/user-attachments/assets/f298a874-bc02-4cc8-9811-2e8adee32c22" />
-
-
-using:
-```py
-import logger
-log=logger.log
-log('text')
-```
-only.
-
 It’s a verbose though.
+
+<img width="737" height="119" alt="console output" src="https://github.com/user-attachments/assets/f298a874-bc02-4cc8-9811-2e8adee32c22" />
+
+If you need help, read the code!  
--- logger.py
+++ logger.py
@@ -1,7 +1,8 @@
-import ntplib
-import time
 import threading
+import ntplib
 import atexit
+import time
+import os
 
 from rich.console import Console
 from rich.color import ANSI_COLOR_NAMES
@@ -56,91 +57,143 @@ class NetTime:
 
 ntime = NetTime()
 plog = Console(highlight=False,markup=False)
+class Logger:
+    LEVELS=["info ","warn ","error"]
+    ERRORSTYLE="dark_red"
+    STYLES=["bright_white","bright_yellow","bright_red"]
 
-LEVELS=["info ","warn ","error"]
-STYLES=["bright_white","bright_yellow","bright_red"]
-ERRORSTYLE="dark_red"
+    def __init__(self,logstyle:str|None=None,outputfile:bool=False,file_path:str|None=None) -> None:
+        """Initialize the Logger object.
+        
+        Parameters
+        ----------
+        logstyle : str | None, optional
+            logstyle  
+                i: info  
+                t: time  
+                e: text  
+            by default None
+        outputfile : bool, optional
+            Whether to output to a file  
+            by default False
+        file_path : str | None, optional
+            file_path  
+            by default None
+        """
+        self.logstyle=logstyle
 
-def stylecolor() -> None:
-    """
-    Outputs colors that can be used in styles. 
-    """
-    for i in ANSI_COLOR_NAMES.keys():
-        log(f'color log color: {i}', 'color', i)
+        ts=time.strftime("%Y-%m-%d-%H")
+        self.outputfile=outputfile
+        if not self.outputfile:
+            self.file_path="Unexpected_log.log"
+        else:
+            if file_path is None: file_path=os.getcwd()
+            filename=ts+".log"
+            full_path=os.path.join(file_path, filename)
+            if os.path.exists(full_path):
+                i=1
+                while True:
+                    new_name=ts+f"({i}).log"
+                    new_path=os.path.join(file_path, new_name)
+                    if not os.path.exists(new_path):
+                        full_path=new_path
+                        break
+                    i+=1
+            self.file_path = full_path
+            self.offset()
 
-def offset() -> float:
-    """
-    Outputs the offset between the Local and the NTP server.
+    def stylecolor(self) -> None:
+        """
+        Outputs colors that can be used in styles. 
+        """
+        for i in ANSI_COLOR_NAMES.keys():
+            self.log(f'color log color: {i}', 'color', i)
 
-    Returns
-    -------
-    float
-        _description_
-    """
-    log(f'Local Time is {time.strftime("%Y:%m:%d:%H:%M:%S")}')
-    log(f'{'Lated' if ntime.offset > 0 else 'Early'}: {ntime.offset:.5}sec')
-    return ntime.offset
+    def offset(self,logput:bool=True,level:int|str=0)  -> tuple[float | str, list[str]]:
+        """
+        Outputs the offset between the Local and the NTP server.
+        
+        Parameters
+        ----------
+        logput : bool, optional
+            Whether to output to the log  
+            by default True
+        level : int | str, optional
+            info level  
+            by default 0
 
-def log2file(text:str) -> None:
-    """
-    ## work in progress
-    Outputs the log to file.
+        Returns
+        -------
+        tuple[float | str, list[str]]
+            rawoffset,[text1,text2]
+        """
+        if logput:
+            self.log(f'Local Time is {time.strftime("%Y:%m:%d:%H:%M:%S")}',level)
+            self.log(f'{'Late' if ntime.offset > 0 else 'Early'}: {abs(ntime.offset):.5f}sec',level)
+        return ntime.offset, [f'Local Time is {time.strftime("%Y:%m:%d:%H:%M:%S")}',f'{'Late' if ntime.offset > 0 else 'Early'}: {abs(ntime.offset):.5f}sec']
 
-    Parameters
-    ----------
-    text : str
-        The string to be output to the log file.
-    """
-    pass
+    def log2file(self,text:str) -> None:
+        """
+        ## work in progress
+        Outputs the log to file.
 
-def log(text,info:int|str=0,style:str|None=None) -> str:
-    """
-    Outputs the log.
+        Parameters
+        ----------
+        text : str
+            The string to be output to the log file.
+        """
+        with open(self.file_path, "a", encoding="utf-8") as f:
+            f.write(text + "\n")
 
-    Parameters
-    ----------
-    text : Any
-        The string to be output.
-    info : int | str, optional
-        Sets the info level.  
-        If a string is provided, it will be inserted into info.  
-        0:info  
-        1:warn  
-        2:error  
-        by default 0
-    style : str, optional
-        Log color.  
-        Only works when info is a string.  
-        by default None
+    def log(self,text,info:int|str=0,style:str|None=None) -> str:
+        """
+        Outputs the log.
 
-    Returns
-    -------
-    str
-        Returns the log contents as a string.  
-        Color information will be lost in the process.
-    """
-    tm=time.strftime("%Y:%m:%d:%H:%M:%S",ntime.now_struct())
-    level=LEVELS[0]
-    if isinstance(info,int) and len(LEVELS)-1 >= info: level,style=LEVELS[info],STYLES[info]
-    text=f'[{tm}] {str(text)}'
-    if isinstance(info,str):
-        ret=f'[{info }] {text}'
-        plog.print(f'[{info }] {text}',style=style)
-    elif info>=0 and info<=2:
-        ret=f'[{level}] {text}'
-        plog.print(f'[{level}] {text}',style=style)
-    else:
-        ret=f"[CONSOLE ERROR] [{tm}] Unknown info number: {info}  ; {text}"
-        plog.print(f"[CONSOLE ERROR] [{tm}] Unknown info number: {info}  ; {text}",style=ERRORSTYLE)
-    return ret
-    
+        Parameters
+        ----------
+        text : Any
+            The string to be output.
+        info : int | str, optional
+            Sets the info level.  
+            If a string is provided, it will be inserted into info.  
+            0:info  
+            1:warn  
+            2:error  
+            by default 0
+        style : str, optional
+            Log color.  
+            Only works when info is a string.  
+            by default None
+
+        Returns
+        -------
+        str
+            Returns the log contents as a string.  
+            Color information will be lost in the process.
+        """
+        tm=time.strftime("%H:%M:%S",ntime.now_struct()) # %Y:%m:%d:%H:%M:%S
+        level=self.LEVELS[0]
+        if isinstance(info,int) and len(self.LEVELS)-1 >= info: level,style=self.LEVELS[info],self.STYLES[info]
+        ftext=f'[{tm}] {str(text)}'
+        if isinstance(info,str):
+            ret=f'[{info }] {text}'
+            plog.print(f'[{info }] {ftext}',style=style)
+        elif info>=0 and info<=2:
+            ret=f'[{level}] {text}'
+            plog.print(f'[{level}] {ftext}',style=style)
+        else:
+            ret=f"[CONSOLE ERROR] [{tm}] Unknown info number: {info}  ; {text}"
+            plog.print(f"[CONSOLE ERROR] [{tm}] Unknown info number: {info}  ; {text}",style=self.ERRORSTYLE)
+        if self.outputfile: self.log2file(ret)
+        return ret
 
 if __name__=='__main__':
-    log('testmessage')
-    log('warn log',1)
-    log('error log',2)
-    log('console error log',3)
-    log('text info log','test ')
-    offset()
+    logger=Logger(outputfile=True)
+    logger.log('testmessage')
+    logger.log('warn log',1)
+    logger.log('error log',2)
+    logger.log('console error log',3)
+    logger.log('text info log','test ')
+    logger.offset()
     if input('Show colors that can be used in styles?(y/n)>')=='y':
-        stylecolor()
+        logger.stylecolor()
